fbfcbce1a41773535bfd2d3f1c775ba0fa8b7874
Alexander Gorbachev <takunarighto@gmail.com>
2018-02-06 15:47:43 +0500

    Post response on client

--- frontend/controllers/bot/commands/PostCommand.php
+++ frontend/controllers/bot/commands/PostCommand.php
@@ -332,6 +332,7 @@ class PostCommand extends UserCommand
                         }
 
                         $data['text'] = "Ваш пост будет опубликован через " . $dif;
+                        $notes['fm'] = Request::sendMessage($data);
 
                         return (new SendpostCommand($this->telegram,
                             new Update(json_decode($this->update->toJson(), true))))->executeNow($data['text']);
