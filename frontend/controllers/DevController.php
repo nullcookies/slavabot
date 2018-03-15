b592844a2cb4b44a396cebf1be19d272b1138dbf
Alexander Gorbachev <takunarighto@gmail.com>
2018-03-15 14:10:00 +0500

    Integration

--- frontend/controllers/DevController.php
+++ frontend/controllers/DevController.php
@@ -11,6 +11,7 @@ use yii\filters\AccessControl;
 use yii\web\Response;
 use common\commands\command\CheckStatusNotificationCommand;
 
+use SoapClient;
 
 
 class DevController extends Controller
@@ -42,7 +43,7 @@ class DevController extends Controller
             ],
             [
                 'class' => \yii\filters\ContentNegotiator::className(),
-                'only' => ['index'],
+                //'only' => ['index'],
                 'formats' => [
                     'application/json' => Response::FORMAT_JSON,
                 ],
@@ -51,6 +52,71 @@ class DevController extends Controller
     }
 
     public function actionIndex(){
-        return User::postingNotification(-1);
+
+        $wsdl = 'http://sm.mlg.ru/services/CubusService.svc?wsdl';
+
+        $trace = true;
+        $exceptions = false;
+
+       // $client = new SoapClient($wsdl, array('trace' => $trace, 'exceptions' => $exceptions));
+
+        $xmlString = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\" xmlns:mlg=\"http://schemas.datacontract.org/2004/07/MlgBuzz.Web.Services\">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <tem:GetPosts>
+                             <!--Optional:-->
+                             <tem:credentials>
+                                <!--Optional:-->
+                                <mlg:Login>leads.im</mlg:Login>
+                                <!--Optional:-->
+                                <mlg:Password>дуфвы1423</mlg:Password>
+                             </tem:credentials>
+                             <!--Optional:-->
+                             <tem:reportId>77357</tem:reportId>
+                             <!--Optional:-->
+                             <tem:dateFrom>2018-03-10T19:00:00.000Z</tem:dateFrom>
+                             <!--Optional:-->
+                             <tem:dateTo>2018-03-14T18:59:59.999Z</tem:dateTo>
+                             <!--Optional:-->
+                             <tem:pageIndex>1</tem:pageIndex>
+                             <!--Optional:-->
+                             <tem:pageSize>100</tem:pageSize>
+                          </tem:GetPosts>
+                       </soapenv:Body>
+                    </soapenv:Envelope>";
+
+        $url = $wsdl;
+
+        $header = array(
+            "Content-type: text/xml;charset=\"utf-8\"",
+            "Accept: text/xml",
+            "Cache-Control: no-cache",
+            "Pragma: no-cache",
+            "SOAPAction: \"http://tempuri.org/ICubusService/GetPosts\"",
+            "Content-length: ".strlen($xmlString),
+            "Host: sm.mlg.ru"
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlString); // the SOAP request
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $xml = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $response);
+        $xml = simplexml_load_string($xml);
+        $json = json_encode($xml);
+        $responseArray = json_decode($json,true);
+        echo '<pre>';
+        print_r($responseArray);
+        echo '</pre>';
+
     }
 }
