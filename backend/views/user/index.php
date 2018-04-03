d95c4d7fef6f4234621d3c8406b35414104a2d95
Alexander Gorbachev <takunarighto@gmail.com>
2018-04-03 10:17:59 +0500

    Users on backend

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'username',
                 'label' => 'Имя пользователя'
             ],
-             'email:email',
+            'email:email',
             [
                 'attribute' => 'telegram_id',
                 'label' => 'Статус Telegram',
@@ -46,15 +46,15 @@ $this->params['breadcrumbs'][] = $this->title;
                 'value' => function ($model, $key, $index, $column) {
 
                     $socials = [
-                            "instagram",
-                            "vkontakte",
-                            "facebook"
+                        "instagram",
+                        "vkontakte",
+                        "facebook"
                     ];
 
                     $str = "";
 
                     $accounts = \yii\helpers\ArrayHelper::getColumn(
-                            $model->dataAccounts,
+                        $model->dataAccounts,
                         'type'
                     );
 
@@ -73,7 +73,105 @@ $this->params['breadcrumbs'][] = $this->title;
                 },
 
             ],
+            [
+                'attribute' => 'tariffValue',
+                'label' => 'Тариф',
+                'format' => 'raw',
 
+                'value' => function ($model, $key, $index, $column) {
+
+                    if($model->tariffValue->tariffValue===null){
+                        return \yii\helpers\Html::tag(
+                            'span',
+                            'Нет данных',
+                            [
+                                'class' => 'label',
+                                'style' => 'background:#d9534f; width: 100%; display: block;'
+                            ]
+                        );
+                    }
+                    $types = [
+                        '' => [
+                            'name'=>'По умолчанию',
+                            'bg' => '#03a9f4'
+                        ],
+                        'green-bg' =>
+                            [
+                                'name'=>'Зеленый',
+                                'bg' => '#8bc34a'
+                            ],
+                        'yellow-bg' =>
+                            [
+                                'name'=>'Желтый',
+                                'bg' => '#ffc107'
+                            ],
+                        'purple-bg' =>
+                            [
+                                'name'=>'Пурпурный',
+                                'bg' => '#9c27b0'
+                            ],
+                    ];
+
+                    return \yii\helpers\Html::tag(
+                        'span',
+                        $model->tariffValue->tariffValue->title,
+                        [
+                            'class' => 'label',
+                            'style' => 'background:'.$types[$model->tariffValue->tariffValue->color]['bg'].'; width: 100%; display: block;'
+                        ]
+                    );
+
+                },
+            ],
+            [
+                'attribute' => 'tariffValue',
+                'label' => 'Остаток',
+                'format' => 'raw',
+
+                'value' => function ($model, $key, $index, $column) {
+
+                    if($model->tariffValue->tariffValue===null){
+                        return \yii\helpers\Html::tag(
+                            'span',
+                            'Нет данных',
+                            [
+                                'class' => 'label',
+                                'style' => 'background:#d9534f; width: 100%; display: block;'
+                            ]
+                        );
+                    }else{
+                        //$color = '#d9534f';
+                        \Carbon\Carbon::setLocale('ru');
+                        $td = \Carbon\Carbon::now()->diff(\Carbon\Carbon::parse($model->tariffValue->expire));
+
+                        $dif = "";
+
+                        if ($td->y > 0) {
+                            $dif .= \frontend\controllers\bot\libs\Utils::human_plural_form($td->y, ["год", "года", "лет"]) . " ";
+                        }
+                        if ($td->m > 0) {
+                            $dif .= \frontend\controllers\bot\libs\Utils::human_plural_form($td->m, ["месяц", "месяц", "месяцев"]) . " ";
+                        }
+                        if ($td->d > 0) {
+                            $dif .= \frontend\controllers\bot\libs\Utils::human_plural_form($td->d, ["день", "дня", "дней"]);
+                        }
+                        if ($td->d == 0 && $td->h > 0) {
+                            $dif .= \frontend\controllers\bot\libs\Utils::human_plural_form($td->h, ["час", "часа", "часов"]);
+                        }
+
+                        //return $dif;
+                        // #d9534f - red
+                        return \yii\helpers\Html::tag(
+                            'span',
+                            $dif,
+                            [
+                                //'class' => 'label',
+                                //'style' => 'background:'.$color.'; width: 100%; display: block;'
+                            ]
+                        );
+                    }
+                },
+            ],
             ['class' => 'yii\grid\ActionColumn', 'template' => '{view}'],
         ],
     ]); ?>
