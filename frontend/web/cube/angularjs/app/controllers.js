f74ced78c4587a6c4fc4e1b4d0bec6abb1a9e1c8
Alexander Gorbachev <takunarighto@gmail.com>
2018-04-06 11:51:20 +0500

    Filters fix

--- frontend/web/cube/angularjs/app/controllers.js
+++ frontend/web/cube/angularjs/app/controllers.js
@@ -509,6 +509,7 @@ angular.module('cubeWebApp')
         $scope.city;
         $scope.country;
         $scope.region;
+        $scope.theme;
 
         $scope.sce = $sce;
 
@@ -579,10 +580,6 @@ angular.module('cubeWebApp')
             $scope.filterName = response.data.filter.name;
             $scope.country = (response.data.filter.aCountry) ? response.data.filter.aCountry.toString() : response.data.filter.aCountry;
             $scope.region = (response.data.filter.aRegion) ? response.data.filter.aRegion.toString() : response.data.filter.aRegion;
-
-
-
-            console.log('Posts request')
 
             $scope.setPage(0);
         });
